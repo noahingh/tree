b76952eec5d1e89268c37517569620cbbbbeeabf
hanjunlee <opsdownn@gmail.com>
2019-12-17 17:50:37 +0900

    fix topology sort

--- topology.go
+++ topology.go
@@ -9,17 +9,22 @@ var (
 	ErrCircuit = fmt.Errorf("Circuit exist in the topology")
 )
 
-func topologicalSort(adj map[int][]int) ([]int, error) {
+// HasCircuit validate the set of adjacents has a circuit or not.
+func HasCircuit(adj [CntItemLimit][CntItemLimit]bool) bool {
+	_, err := topologicalSort(adj)
+	if err != nil {
+		return true
+	}
+	return false
+}
+
+func topologicalSort(adj [CntItemLimit][CntItemLimit]bool) ([]int, error) {
 	var (
 		sorted  = []int{}
-		visited = map[int]bool{}
+		visited = make([]bool, CntItemLimit)
 	)
 
-	for from := range adj {
-		visited[from] = false
-	}
-
-	for from := range adj {
+	for from := 0; from < CntItemLimit; from++ {
 		if visited[from] {
 			continue
 		}
@@ -38,7 +43,7 @@ func topologicalSort(adj map[int][]int) ([]int, error) {
 }
 
 // visit visit all adjacent vertex as the depth first search.
-func visit(from int, adj map[int][]int, visited map[int]bool) []int {
+func visit(from int, adj [CntItemLimit][CntItemLimit]bool, visited []bool) []int {
 	var (
 		output []int
 	)
@@ -46,26 +51,23 @@ func visit(from int, adj map[int][]int, visited map[int]bool) []int {
 	// set visited
 	visited[from] = true
 
-	tos := adj[from]
-	for _, to := range tos {
-		if visited[to] {
-			continue
+	for to := 0; to < CntItemLimit; to++ {
+		if adj[from][to] && !visited[to] {
+			o := visit(to, adj, visited)
+			output = append(output, o...)
 		}
-
-		o := visit(to, adj, visited)
-		output = append(output, o...)
 	}
 
 	output = append(output, from)
 	return output
 }
 
-func hasCircuit(sorted []int, adj map[int][]int) bool {
+func hasCircuit(sorted []int, adj [CntItemLimit][CntItemLimit]bool) bool {
 	for right := len(sorted) - 1; right >= 1; right-- {
 		for left := right - 1; left >= 0; left-- {
 			from, to := sorted[right], sorted[left]
 
-			if tos := adj[from]; searchInt(tos, to) {
+			if adj[from][to] {
 				return true
 			}
 		}
@@ -73,17 +75,8 @@ func hasCircuit(sorted []int, adj map[int][]int) bool {
 	return false
 }
 
-func searchInt(ints []int, x int) bool {
-	for _, i := range ints {
-		if i == x {
-			return true
-		}
-	}
-	return false
-}
-
 func reverse(ints []int) (ret []int) {
-	for i := len(ints)-1; i >= 0; i-- {
+	for i := len(ints) - 1; i >= 0; i-- {
 		ret = append(ret, ints[i])
 	}
 	return ret
--- topology_test.go
+++ topology_test.go
@@ -5,148 +5,9 @@ import (
 	"testing"
 )
 
-// NormalCase return a successful case.
-// 0
-// └── 1 
-//     ├── 2 
-// 	   └── 3
-func NormalCase() (sorted []int, adj map[int][]int) {
-	sorted = []int {0, 1, 3, 2}
-	adj = map[int][]int{
-		0: []int{1},
-		1: []int{2, 3},
-		2: []int{},
-		3: []int{},
-	}
-	return sorted, adj
-}
-
-// CircuitCase return a circuit case.
-// 0
-// └── 1 
-//     └── 0
-func CircuitCase() (sorted []int, adj map[int][]int) {
-	sorted = []int{0, 1}
-	adj = map[int][]int{
-		0: []int{1},
-		1: []int{0},
-	}
-	return sorted, adj
-}
-
-// ComplexCase return a complex case.
-// 0
-// ├── 1
-// │   ├── 2
-// │   └── 3
-// └── 4
-//     └── 5
-func ComplexCase() (sorted []int, adj map[int][]int) {
-	sorted = []int{0, 4, 5, 1, 3, 2}
-	adj = map[int][]int{
-		0: []int{1, 4},
-		1: []int{2, 3},
-		2: []int{},
-		3: []int{},
-		4: []int{5},
-		5: []int{},
-	}
-
-	return sorted, adj
-}
-
-func TestTopologicalSort(t *testing.T) {
-	ns, na := NormalCase()
-	_, ca := CircuitCase()
-	ccs, cca := ComplexCase()
-
-	type args struct {
-		adj map[int][]int
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    []int
-		wantErr bool
-	}{
-		{
-			name: "normal case",
-			args: args{
-				adj: na,
-			},
-			want: ns,
-			wantErr: false,
-		},
-		{
-			name: "circuit case",
-			args: args{
-				adj: ca,
-			},
-			want: []int{},		// should get a empty slice.
-			wantErr: true,
-		},
-		{
-			name: "complex case",
-			args: args{
-				adj: cca,
-			},
-			want: ccs,
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := topologicalSort(tt.args.adj)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("topologicalSort() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("topologicalSort() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestVisit(t *testing.T) {
-	_, na := NormalCase()
-	type args struct {
-		from    int
-		adj     map[int][]int
-		visited map[int]bool
-	}
-	tests := []struct {
-		name string
-		args args
-		want []int
-	}{
-		{
-			name: "normal case",
-			args: args{
-				from: 0,
-				adj: na,
-				visited: map[int]bool{0: false, 1: false, 2: false, 3: false},
-			},
-			want: []int {2, 3, 1, 0},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := visit(tt.args.from, tt.args.adj, tt.args.visited); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("visit() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestHasCircuit(t *testing.T) {
-	ns, na := NormalCase()
-	cs, ca := CircuitCase()
-	ccs, cca := ComplexCase()
-
 	type args struct {
-		sorted []int
-		adj    map[int][]int
+		adj [CntItemLimit][CntItemLimit]bool
 	}
 	tests := []struct {
 		name string
@@ -154,34 +15,110 @@ func TestHasCircuit(t *testing.T) {
 		want bool
 	}{
 		{
-			name: "normal case",
+			name: "circuit",
 			args: args{
-				sorted: ns,
-				adj: na,			
-			},
-			want: false,
-		},
-		{
-			name: "circuit case",
-			args: args{
-				sorted: cs,
-				adj: ca,
+				adj: [CntItemLimit][CntItemLimit]bool{
+					{false, true},
+					{true, false},
+				},
 			},
 			want: true,
 		},
 		{
-			name: "complex case",
+			name: "step",
 			args: args{
-				sorted: ccs,
-				adj: cca,
+				adj: [CntItemLimit][CntItemLimit]bool{
+					{false, true},
+					{false, false, true},
+					{false, false, false, true},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "file system",
+			args: args{
+				adj: [CntItemLimit][CntItemLimit]bool{
+					{false, true},
+					{false, false, true, true}, // 1 -> 2,3
+					{false, false, false, false},
+					{false, false, false, false},
+					{false, false, false, false, false, true}, // 4 -> 5
+					{false, false, false, false, false, false},
+				},
 			},
 			want: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if got := HasCircuit(tt.args.adj); got != tt.want {
+				t.Errorf("HasCircuit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+
+func Test_hasCircuit(t *testing.T) {
+	type args struct {
+		sorted []int
+		adj    [CntItemLimit][CntItemLimit]bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "circuit exist",
+			args: args{
+				sorted: []int{0, 1},
+				adj: [CntItemLimit][CntItemLimit]bool{
+					{false, true},
+					{true, false},
+				},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			if got := hasCircuit(tt.args.sorted, tt.args.adj); got != tt.want {
 				t.Errorf("hasCircuit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_reverse(t *testing.T) {
+	type args struct {
+		ints []int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantRet []int
+	}{
+		{
+			name: "increasing slice",
+			args: args{
+				ints: []int{0, 1, 2, 3, 4},
+			},
+			wantRet: []int{4, 3, 2, 1, 0},
+		},
+		{
+			name: "decreasing slice",
+			args: args{
+				ints: []int{4, 3, 2, 1, 0},
+			},
+			wantRet: []int{0, 1, 2, 3, 4},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRet := reverse(tt.args.ints); !reflect.DeepEqual(gotRet, tt.wantRet) {
+				t.Errorf("reverse() = %v, want %v", gotRet, tt.wantRet)
 			}
 		})
 	}
